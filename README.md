394c773f394a9df6384267a73cfbdb75aedbb3ca
Jason Cosper <boogah@gmail.com>
2017-10-19 11:07:07 -0700

    Add plugin, update README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Gutenag
 
-Quiet the "try Guternberg" nag introduced in [WordPress 4.9 beta 3](https://wordpress.org/news/2017/10/wordpress-4-9-beta-3/).
+This simple plugin hides the "try Gutenberg" nag in the WordPress dashboard. This feature was introduced in [WordPress 4.9 beta 3](https://wordpress.org/news/2017/10/wordpress-4-9-beta-3/) and is expected to land in WordPress 4.9 when it is released in November of 2017.
---
+++ gutenag.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Gutenag
+ *
+ * Gutenag is a plugin that quiets the prompt to install the new post
+ * editor in WordPress, Gutenberg.
+ *
+ * @link              https://github.com/boogah/gutenag/
+ * @package           Gutenag
+ * @author            Jason Cosper <boogah@gmail.com>
+ * @license           GPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Gutenag
+ * Plugin URI:        https://github.com/boogah/gutenag/
+ * Description:       Hides the "try Gutenberg" nag in WordPress 4.9
+ * Version:           1.0.0
+ * Author:            Jason Cosper
+ * Author URI:        https://jasoncosper.com/
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Hide the "try Gutenberg" nag.
+ *
+ * @since 1.0.0
+ */
+remove_action( 'try_gutenberg_panel', 'wp_try_gutenberg_panel' );
